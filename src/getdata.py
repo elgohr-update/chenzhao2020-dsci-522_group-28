9459db0dafd41b5d7db69e86675dae698b893e3d
Debananda Sarkar <sarkar.debananda.1@gmail.com>
2020-11-21 00:39:13 -0600

    This commit will fix path issue in getdata.py

--- src/getdata.py
+++ src/getdata.py
@@ -19,14 +19,13 @@ opt = docopt(__doc__)
 def main(source_url, target_file):
     try:
         hotels_dataset = pd.read_csv(source_url)
-        hotels_dataset.to_csv("data/raw/hotels_dataset.csv")
+        hotels_dataset.to_csv(target_file, index=False)
         print("Download complete")
     except FileNotFoundError as fx:
         print("Error in target file path")
         print(fx)
         print(type(fx))   
     except Exception as ex:
-        print("Error fetching data from source. Check the url")
         print(ex)
         print(type(ex))
 
