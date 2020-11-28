b502a5e007dd61f082010700a52e357e5c7c4738
Chen Zhao <chenzhaouc@gmail.com>
2020-11-28 14:28:26 -0700

    Modify runall file and delete unnecessary files

--- runall.sh
+++ runall.sh
@@ -13,6 +13,7 @@ python src/getdata.py --source_url=https://raw.githubusercontent.com/rfordatasci
 python src/split_dataset_train_test.py --source_data_file=data/raw/hotels_dataset.csv --train_data_file=data/processed/train_df.csv --test_data_file=data/processed/test_df.csv --test_split_ratio=0.2 --chosen_seed=2020
 
 # create exploratory data analysis tables and figures, and write to file
+python src/eda_ms2.py --train=data/processed/train_df.csv --out_dir=results/
 
 # train model and tune hyperparameters
 python src/classifier_evaluation.py data/processed/train_df.csv reports/two_fold_cross_validation_result.csv --n_cv_folds=2 --chosen_seed=2020 --verbose=True
